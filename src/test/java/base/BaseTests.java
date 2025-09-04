7395798adf1d22fa84db0fa92aaf769259fdda3e
Luca Vinelli <lucavinelli4@gmail.com>
2025-09-04 13:34:00 -0300

    chore: Initial commit of the UI automation framework

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,41 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.HomePage;
+
+public class BaseTests {
+
+    protected WebDriver driver;
+    protected HomePage homePage;
+
+    @BeforeMethod
+    public void setUp(){
+        String browser  = System.getProperty("browser", "chrome");
+        boolean headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+        String baseUrl  = System.getProperty("baseUrl", "https://osvaldovinelli.com.ar/");
+
+        if ("chrome".equalsIgnoreCase(browser)) {
+            io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
+            org.openqa.selenium.chrome.ChromeOptions options = new org.openqa.selenium.chrome.ChromeOptions();
+            if (headless) {
+                options.addArguments("--headless=new");
+                options.addArguments("--window-size=1920,1080");
+            }
+            driver = new org.openqa.selenium.chrome.ChromeDriver(options);
+        } else {
+            throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+        homePage = new HomePage(driver);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
